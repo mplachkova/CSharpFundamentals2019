bb1e23d9ca4fdd38e408d0d941f073270272966a
Maria <52293371+mumuzette@users.noreply.github.com>
2019-10-12 18:14:54 +0300

    Update factorialDivision.cs

--- factorialDivision.cs
+++ factorialDivision.cs
@@ -10,18 +10,18 @@ namespace factorialDivision
             //Read two integer numbers.
             //Calculate factorial of each number.
             //Divide the first result by the second and print the division formatted to the second decimal point.
-            ulong firstFactorialBase = ulong.Parse(Console.ReadLine());
-            ulong secondFactorialBase = ulong.Parse(Console.ReadLine());
-            ulong firstFactorial = FactorialReturn(firstFactorialBase);
-            ulong secondFactorial = FactorialReturn(secondFactorialBase);
+            long firstFactorialBase = long.Parse(Console.ReadLine());
+            long secondFactorialBase = long.Parse(Console.ReadLine());
+            long firstFactorial = FactorialReturn(firstFactorialBase);
+            long secondFactorial = FactorialReturn(secondFactorialBase);
             double result = 1.0 * firstFactorial / secondFactorial;
             Console.WriteLine($"{result:f2}");
         }
 
-        static ulong FactorialReturn(ulong factorialBase)
+        static long FactorialReturn(long factorialBase)
         {
-            ulong factorial = 1;
-            for (ulong i = 1; i <= factorialBase; i++)
+            long factorial = 1;
+            for (long i = 1; i <= factorialBase; i++)
             {
                 factorial *= i;
             }
