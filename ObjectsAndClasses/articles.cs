f714b6e21238e6aae73e7e68861ebebf7d6c2b19
Maria <52293371+mumuzette@users.noreply.github.com>
2019-11-08 22:07:28 +0200

    Add files via upload

---
+++ ObjectsAndClasses/articles.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace articles
+{
+    class articles
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ");
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            Article myArticle = new Article(input[0], input[1], input[2]);
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] instructions = Console.ReadLine().Split(": ");
+                string command = instructions[0];
+                string newData = instructions[1];
+                switch(command)
+                {
+                    case "Edit":
+                        myArticle.Edit(newData);
+                        break;
+                    case "ChangeAuthor":
+                        myArticle.ChangeAuthor(newData);
+                        break;
+                    case "Rename":
+                        myArticle.Rename(newData);
+                        break;
+                }
+            }
+            Console.WriteLine(myArticle.ToString());
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public void ChangeAuthor(string newAuthor)
+        {
+            Author = newAuthor;
+        }
+
+        public void Rename(string newTitle)
+        {
+            Title = newTitle;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Title} - {Content}: {Author}";
+            return result;
+        }
+    }
+}
---
+++ ObjectsAndClasses/articles_Two.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace articles
+{
+    class articles
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<Article> articleList = new List<Article>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] articleProperties = Console.ReadLine().Split(", ");
+                string articleTitle = articleProperties[0];
+                string articleContent = articleProperties[1];
+                string articleAuthor = articleProperties[2];
+                Article myArticle = new Article(articleProperties[0], articleProperties[1], articleProperties[2]);
+                articleList.Add(myArticle);
+            }
+            string orderByArgument = Console.ReadLine();
+            switch (orderByArgument)
+            {
+                case "title":
+                    articleList = articleList.OrderBy(article => article.Title).ToList();
+                    break;
+                case "content":
+                    articleList = articleList.OrderBy(article => article.Content).ToList();
+                    break;
+                case "author":
+                    articleList = articleList.OrderBy(article => article.Author).ToList();
+                    break;
+            }
+            foreach (Article element in articleList)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+
+        public void Edit(string newContent)
+        {
+            Content = newContent;
+        }
+
+        public override string ToString()
+        {
+            string result = $"{Title} - {Content}: {Author}";
+            return result;
+        }
+    }
+}
