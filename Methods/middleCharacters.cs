0c86e42c55456fc1834e72aabbade7886e6b8c26
Maria <52293371+mumuzette@users.noreply.github.com>
2019-11-08 21:45:58 +0200

    Add files via upload

---
+++ Methods/middleCharacters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace middleCharacters
+{
+    class middleCharacters
+    {
+        static void Main(string[] args)
+        {
+            //You will receive a single string.Write a method that prints the middle character.
+            //If the length of the string is even there are two middle characters.
+            string inputString = Console.ReadLine();
+            ReturnTheMiddleChar(inputString);
+        }
+
+        static void ReturnTheMiddleChar(string text)
+        {
+            int lengthOfInput = text.Length;
+            int loopLength = lengthOfInput / 2;
+            if (lengthOfInput % 2 != 0)
+            {
+                Console.WriteLine(text[loopLength]);
+            }
+            else
+            {
+                Console.WriteLine(text[loopLength - 1].ToString() + text[loopLength].ToString());
+            }
+        }
+    }
+}
